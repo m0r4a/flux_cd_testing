d017e963a905952b53f7a3ce7438281fc256ec28
Gael Mora <mora@proton.me>
2026-03-11 04:41:50 +0000

    Remove namespace resource from grafana base

--- bootstrap.sh
+++
@@ -1,9 +0,0 @@
-flux install
-
-kubectl create ns flux-system
-
-kubectl apply -f \
-https://raw.githubusercontent.com/m0r4a/flux_cd_testing/main/clusters/homelab/flux-system/gotk-components.yaml
-
-kubectl apply -f \
-https://raw.githubusercontent.com/m0r4a/flux_cd_testing/main/clusters/homelab/flux-system/gotk-sync.yaml
